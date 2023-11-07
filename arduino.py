358b3c280aa7e3eb5b9a6a7bc479cd6f91eab78c
Hartansky,Lukas L <l.hartansky@student.fontys.nl>
2023-11-07 12:23:45 +0100

    arduino prototype(still needs some upgrades)

--- arduino.py
+++ arduino.py
@@ -1,68 +1,94 @@
-import serial
-import time
-import pyfirmata
+import time, sys
+import requests
+from fhict_cb_01.custom_telemetrix import CustomTelemetrix
+board = CustomTelemetrix() 
 
-# Set the port where your Arduino is connected
-# On Windows, it should look like 'COM3' or similar
-port = 'COM3'  # Change this to your port
 
-try:
-    # Open a serial connection to the Arduino
-    ser = serial.Serial(port, baudrate=57600, timeout=1)
 
-    print(ser.readline().decode('utf-8')) 
+BUTTON1PIN = 8
+BUTTON2PIN = 9
+BUZZER = 3
+pin_red = LEDPINS= 4
+pin_green = LEDPINS= 5
+pin_blue = LEDPINS=6
+pin_yellow = LEDPINS= 7
+trigger = 0
+add_time = 0
+def safetycheck(safety_break):
+    global trigger
+    trigger = safety_break[2]
+def set_timer(time_up):
+    global add_time
+    add_time = time_up[2] 
+def setup():
+    board.set_pin_mode_digital_input_pullup(BUTTON1PIN, callback=safetycheck)
+    board.set_pin_mode_digital_input_pullup(BUTTON2PIN, callback=set_timer)
+    board.set_pin_mode_digital_output(BUZZER)
+    board.displayOn()
 
-    # Define pin for the speaker
-    speaker_pin = 9  # Change this to the pin where your speaker is connected
-
-    # Define the countdown time in seconds for the oven
-    oven_countdown_time = 5  # 5 minutes for demonstration, change as needed
-
-    # Function to start the oven countdown with music
-    def start_oven_countdown_with_music(countdown_time):
-        print("Oven countdown with music started!")
-
-        # Send a message to the Arduino to start the countdown with music
-        ser.write(b'START_WITH_MUSIC\n')
-
-        for i in range(countdown_time, 0, -1):
-            print("Time remaining:", i)
-            time.sleep(1)
-
-        # Send a message to the Arduino to stop the music
-        ser.write(b'STOP_MUSIC\n')
-
-        print("Oven countdown with music complete!")
-
-        # Close the serial connection
-        ser.close()
-
-    # Start the oven countdown with music
-    start_oven_countdown_with_music(oven_countdown_time)
-
-except Exception as e:
-    print(f"An error occurred: {e}")
-
-arduino_port = 'COM3'  # Update with your port
-baud_rate = 9600
-
-try:
-    # Open a serial connection to the Arduino
-    ser = serial.Serial(arduino_port, baudrate=baud_rate, timeout=1)
-
-    # Define pin for the buzzer
-    buzzer_pin = 9  # Change this to the pin where your buzzer is connected
-
-    while True:
-        # Send 1KHz sound signal for 1 second
-        ser.write(b'START\n')
-        time.sleep(1)
-
-        # Stop sound for 1 second
-        ser.write(b'STOP\n')
-        time.sleep(1)
-
-except KeyboardInterrupt:
-    # Clean up when the program is interrupted
-    ser.close()
     
+def loop():
+    time.sleep(1)
+    board.displayShow('0')
+    board.digital_write(pin_green, 1)
+    Timer = 0
+    while trigger !=0:
+        board.digital_read(BUTTON1PIN)
+        time.sleep(0.5)
+    
+    Pizza_ready= 'Pizza is not ready sir Luigi'
+    arduino_data = {"Pizza": Pizza_ready, }
+    #response = requests.post('http://localhost:5000/update_arduino', json = arduino_data)
+
+    board.digital_write(pin_green, 0)
+    board.digital_write(pin_blue, 1)
+    while add_time !=0:
+        board.displayShow(Timer)
+        Timer +=10
+        minutes = Timer // 60
+        seconds = Timer % 60
+        display_time = '{:02d}.{:02d}'.format(minutes, seconds)
+        board.displayShow(display_time)
+        board.digital_read(BUTTON2PIN)
+        time.sleep(0.5)
+
+    board.digital_write(pin_yellow, 1)
+    board.digital_write(pin_blue, 0)
+    print('Press the button to start the timer Sir Luigi')
+    while trigger != 0:
+        board.digital_read(BUTTON1PIN)
+        time.sleep(0.5)
+
+    while Timer !=0 and add_time!=0:
+        board.digital_write(pin_red, 1)
+        board.digital_write(pin_yellow, 0)
+        minutes = Timer // 60
+        seconds = Timer % 60
+        display_time = '{:02d}.{:02d}'.format(minutes, seconds)
+        Timer -=1
+        time.sleep(1)
+        board.displayShow(display_time)
+
+    board.displayShow('0000')
+    board.digital_write(pin_green, 1)
+    board.digital_write(pin_red, 0)
+    board.digital_write(BUZZER, 1)
+    Pizza_ready= 'Pizza is ready sir Luigi'
+    time.sleep(5)
+    board.digital_write(BUZZER, 0) 
+        
+        
+    arduino_data = {"Pizza": Pizza_ready, }
+   # response = requests.post('http://localhost:5000/update_arduino', json = arduino_data)
+    time.sleep(6)
+
+
+
+setup()
+while True:
+    try:
+        loop()
+    except KeyboardInterrupt: # Shutdown Firmata on Crtl+C.
+        print ('Bye Bye')
+        board.shutdown()
+        sys.exit(0)  
