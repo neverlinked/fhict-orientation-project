ab06777b9c1b7d9ac5740817112cc7c18983bbea
Hartansky <149473181+Hartansky@users.noreply.github.com>
2023-11-08 13:38:12 +0100

    Revert "test"
    This reverts commit b318676fe31b5faa60f194f973e3429a23bf5c40.

--- app.py
+++ app.py
@@ -1,122 +1,117 @@
-import time
-import sys
-import requests
-from fhict_cb_01.custom_telemetrix import CustomTelemetrix
+from flask import Flask, render_template, request, redirect, url_for
+from collections import deque
 
-board = CustomTelemetrix()
+app = Flask(__name__)
 
-BUTTON1PIN = 8
-BUTTON2PIN = 9
-BUZZER = 3
-pin_red = 4
-pin_green = 5
-pin_blue = 6
-pin_yellow = 7
-trigger = 0
-add_time = 0
+orders = deque()
+order_index = 0  
 
-# Define the URL for your kitchen server
-kitchen_server_url = 'http://10.0.0.191:5003'
+@app.route('/')
+def main():
+    return render_template('index.html')
 
-def safetycheck(safety_break):
-    global trigger
-    trigger = safety_break[2]
+@app.route('/website_main')
+def website_main():
+    return render_template('website_main.html')
 
-def set_timer(time_up):
-    global add_time
-    add_time = time_up[2]
+@app.route('/menu')
+def menu():
+    return render_template('menu.html')
 
-def update_status_on_kitchen_server(order_index, status):
-    # Send a POST request to update the status of an order on the kitchen server
-    payload = {'order_index': order_index, 'status': status}
-    response = requests.post(f'{kitchen_server_url}/update_status', data=payload)
+@app.route('/cart')
+def cart():
+    return render_template('cart.html')
 
-def fetch_orders_from_server():
-    # Fetch orders from the server (use the endpoint defined in your Flask app)
-    response = requests.get(f'{kitchen_server_url}/get_orders')
-    if response.status_code == 200:
-        orders = response.json()
-        return orders
-    else:
-        print("Failed to fetch orders from the server")
-        return []
+@app.route('/cashier')
+def cashier():
+    return render_template('cashier.html', orders=orders)
 
-def setup():
-    board.set_pin_mode_digital_input_pullup(BUTTON1PIN, callback=safetycheck)
-    board.set_pin_mode_digital_input_pullup(BUTTON2PIN, callback=set_timer)
-    board.set_pin_mode_digital_output(BUZZER)
-    board.displayOn()
+@app.route('/kitchen')
+def kitchen():
+    return render_template('kitchen.html', orders=orders)
 
-def loop():
-    time.sleep(1)
-    board.displayShow('0')
-    board.digital_write(pin_green, 1)
-    Timer = 0
+@app.route('/add_order', methods=['POST'])
+def add_order():
+    global order_index  
+    table_number = request.form['table_number']
+    item1 = int(request.form['Margherita'])
+    item2 = int(request.form['Pepperoni'])
+    item3 = int(request.form['Marinara'])
+    item4 = int(request.form['Meatlovers'])
+    item5 = int(request.form['Coca Cola'])
+    item6 = int(request.form['Water Bottle'])
+    total_price = item1 * 12.99 + item2 * 14.99 + item3 * 10.99 + item4 * 19.99 + item5 * 3.99 + item6 * 1.99
+    
+    order = {
+        'table_number': table_number,
+        'items': {
+            'Margherita': item1,
+            'Pepperoni': item2,
+            'Marinara': item3,
+            'Meatlovers': item4,
+            'Coca Cola': item5,
+            'Water Bottle': item6
+        },
+        'status': 'pending',
+        'total_price': total_price
+    }
+    
+    orders.append(order)
+    order_index += 1  
+    return redirect(url_for('cashier'))
 
-    while trigger != 0:
-        board.digital_read(BUTTON1PIN)
-        time.sleep(0.5)
+@app.route('/send_order', methods=['POST'])
+def send_order():
+    global order_index  
+    table_number = request.form['table_number']
+    item1 = int(request.form['Margherita'])
+    item2 = int(request.form['Pepperoni'])
+    item3 = int(request.form['Marinara'])
+    item4 = int(request.form['Meatlovers'])
+    item5 = int(request.form['Coca Cola'])
+    item6 = int(request.form['Water Bottle'])
+    total_price = item1 * 12.99 + item2 * 14.99 + item3 * 10.99 + item4 * 19.99 + item5 * 3.99 + item6 * 1.99
+    
+    order = {
+        'table_number': table_number,
+        'items': {
+            'Margherita': item1,
+            'Pepperoni': item2,
+            'Marinara': item3,
+            'Meatlovers': item4,
+            'Coca Cola': item5,
+            'Water Bottle': item6
+        },
+        'status': 'pending',
+        'total_price': total_price
+    }
+    
+    orders.append(order)
+    order_index += 1  
+    return redirect(url_for('cart'))
 
-    board.digital_write(pin_green, 0)
-    board.digital_write(pin_blue, 1)
+@app.route('/update_status', methods=['POST'])
+def update_status():
+    order_index = int(request.form['order_index'])
+    
+    if 0 <= order_index < len(orders):
+        current_status = orders[order_index]['status']
+        
+        
+        if current_status == 'pending':
+            orders[order_index]['status'] = 'preparation'
+        elif current_status == 'preparation':
+            orders[order_index]['status'] = 'done'
+    
+    return redirect(url_for('kitchen'))
 
-    while add_time != 0:
-        board.displayShow(Timer)
-        Timer += 1
-        minutes = Timer // 60
-        seconds = Timer % 60
-        display_time = '{:02d}.{:02d}'.format(minutes, seconds)
-        board.displayShow(display_time)
-        board.digital_read(BUTTON2PIN)
-        time.sleep(0.5)
-
-    board.digital_write(pin_yellow, 1)
-    board.digital_write(pin_blue, 0)
-    print('Press the button to start the timer Sir Luigi')
-
-    while trigger != 0:
-        board.digital_read(BUTTON1PIN)
-        time.sleep(0.2)
-
-    orders = fetch_orders_from_server()
-
-    if not orders:
-        print("No orders available.")
-    else:
-        first_order = orders[0]  # Assuming orders is a list of dictionaries
-        order_index = first_order['order_index']
-
-        while Timer != 0 and add_time != 0:
-            board.digital_write(pin_red, 1)
-            board.digital_write(pin_yellow, 0)
-            minutes = Timer // 60
-            seconds = Timer % 60
-            display_time = '{:02d}.{:02d}'.format(minutes, seconds)
-            Timer -= 1
-
-            # Check if Timer is greater than 0, and change the order status to "In Preparation"
-            if Timer > 0:
-                update_status_on_kitchen_server(order_index, 'in_preparation')
-
-            time.sleep(1)
-            board.displayShow(display_time)
-
-        board.displayShow('0000')
-        board.digital_write(pin_green, 1)
-        board.digital_write(pin_red, 0)
-        board.digital_write(BUZZER, 1)
-        time.sleep(5)
-        board.digital_write(BUZZER, 0)
-
-        # Change the order status to "Done" when Timer reaches 0
-        update_status_on_kitchen_server(order_index, 'done')
+@app.route('/remove_order', methods=['POST'])
+def remove_order():
+    order_index = int(request.form['order_index'])
+    if 0 <= order_index < len(orders):
+        orders.remove(orders[order_index])
+    
+    return redirect(url_for('main'))
 
 if __name__ == '__main__':
-    setup()
-    while True:
-        try:
-            loop()
-        except KeyboardInterrupt:
-            print('Bye Bye')
-            board.shutdown()
-            sys.exit(0)
+    app.run(debug=True, port=5003)
--- arduino.py
+++ arduino.py
@@ -1,8 +1,6 @@
-import time
-import sys
+import time, sys
 import requests
 from fhict_cb_01.custom_telemetrix import CustomTelemetrix
-
 board = CustomTelemetrix()
 
 BUTTON1PIN = 8
@@ -16,7 +14,7 @@ trigger = 0
 add_time = 0
 
 # Define the URL for your kitchen server
-kitchen_server_url = 'http://10.0.0.191:5003'
+kitchen_server_url = 'http://10.0.0.191:5003/update_status'
 
 def safetycheck(safety_break):
     global trigger
@@ -24,23 +22,14 @@ def safetycheck(safety_break):
 
 def set_timer(time_up):
     global add_time
-    add_time = time_up[2]
+    add_time = time_up[2] 
 
-def update_status_on_kitchen_server(order_index, status):
-    # Send a POST request to update the status of an order on the kitchen server
-    payload = {'order_index': order_index, 'status': status}
-    response = requests.post(f'{kitchen_server_url}/update_status', data=payload)
-
-def fetch_orders_from_server():
-    # Fetch orders from the server (assuming it's in JSON format)
-    response = requests.get(f'{kitchen_server_url}/get_orders')
-    if response.status_code == 200:
-        orders = response.json()
-        return orders
-    else:
-        print("Failed to fetch orders from the server")
-        return []
-
+def update_status_on_kitchen_server(status):
+    # Send a POST request to update the status of the first item on the kitchen server
+    payload = {'order_index': 0, 'status': status}
+    response = requests.post(kitchen_server_url, data=payload)
+    
+    
 def setup():
     board.set_pin_mode_digital_input_pullup(BUTTON1PIN, callback=safetycheck)
     board.set_pin_mode_digital_input_pullup(BUTTON2PIN, callback=set_timer)
@@ -78,38 +67,30 @@ def loop():
         board.digital_read(BUTTON1PIN)
         time.sleep(0.2)
 
-    orders = fetch_orders_from_server()
+    while Timer != 0 and add_time != 0:
+        board.digital_write(pin_red, 1)
+        board.digital_write(pin_yellow, 0)
+        minutes = Timer // 60
+        seconds = Timer % 60
+        display_time = '{:02d}.{:02d}'.format(minutes, seconds)
+        Timer -= 1
+        
+        # Check if Timer is greater than 0, and change the order status to "In Preparation"
+        if Timer > 0:
+            update_status_on_kitchen_server('in_preparation')
+            
+        time.sleep(1)
+        board.displayShow(display_time)
 
-    if not orders:
-        print("No orders available.")
-    else:
-        first_order = orders[0]  # Assuming orders is a list of dictionaries
-        order_index = first_order['order_index']
-
-        while Timer != 0 and add_time != 0:
-            board.digital_write(pin_red, 1)
-            board.digital_write(pin_yellow, 0)
-            minutes = Timer // 60
-            seconds = Timer % 60
-            display_time = '{:02d}.{:02d}'.format(minutes, seconds)
-            Timer -= 1
-
-            # Check if Timer is greater than 0, and change the order status to "In Preparation"
-            if Timer > 0:
-                update_status_on_kitchen_server(order_index, 'in_preparation')
-
-            time.sleep(1)
-            board.displayShow(display_time)
-
-        board.displayShow('0000')
-        board.digital_write(pin_green, 1)
-        board.digital_write(pin_red, 0)
-        board.digital_write(BUZZER, 1)
-        time.sleep(5)
-        board.digital_write(BUZZER, 0)
-
-        # Change the order status to "Done" when Timer reaches 0
-        update_status_on_kitchen_server(order_index, 'done')
+    board.displayShow('0000')
+    board.digital_write(pin_green, 1)
+    board.digital_write(pin_red, 0)
+    board.digital_write(BUZZER, 1)
+    time.sleep(5)
+    board.digital_write(BUZZER, 0)
+    
+    # Change the order status to "Done" when Timer reaches 0
+    update_status_on_kitchen_server('done')
 
 if __name__ == '__main__':
     setup()
@@ -117,6 +98,6 @@ if __name__ == '__main__':
         try:
             loop()
         except KeyboardInterrupt:
-            print('Bye Bye')
+            print ('Bye Bye')
             board.shutdown()
             sys.exit(0)
