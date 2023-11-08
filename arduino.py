b318676fe31b5faa60f194f973e3429a23bf5c40
Hartansky <149473181+Hartansky@users.noreply.github.com>
2023-11-08 13:36:36 +0100

    test

--- arduino.py
+++ arduino.py
@@ -1,6 +1,8 @@
-import time, sys
+import time
+import sys
 import requests
 from fhict_cb_01.custom_telemetrix import CustomTelemetrix
+
 board = CustomTelemetrix()
 
 BUTTON1PIN = 8
@@ -14,7 +16,7 @@ trigger = 0
 add_time = 0
 
 # Define the URL for your kitchen server
-kitchen_server_url = 'http://10.0.0.191:5003/update_status'
+kitchen_server_url = 'http://10.0.0.191:5003'
 
 def safetycheck(safety_break):
     global trigger
@@ -22,14 +24,23 @@ def safetycheck(safety_break):
 
 def set_timer(time_up):
     global add_time
-    add_time = time_up[2] 
+    add_time = time_up[2]
 
-def update_status_on_kitchen_server(status):
-    # Send a POST request to update the status of the first item on the kitchen server
-    payload = {'order_index': 0, 'status': status}
-    response = requests.post(kitchen_server_url, data=payload)
-    
-    
+def update_status_on_kitchen_server(order_index, status):
+    # Send a POST request to update the status of an order on the kitchen server
+    payload = {'order_index': order_index, 'status': status}
+    response = requests.post(f'{kitchen_server_url}/update_status', data=payload)
+
+def fetch_orders_from_server():
+    # Fetch orders from the server (assuming it's in JSON format)
+    response = requests.get(f'{kitchen_server_url}/get_orders')
+    if response.status_code == 200:
+        orders = response.json()
+        return orders
+    else:
+        print("Failed to fetch orders from the server")
+        return []
+
 def setup():
     board.set_pin_mode_digital_input_pullup(BUTTON1PIN, callback=safetycheck)
     board.set_pin_mode_digital_input_pullup(BUTTON2PIN, callback=set_timer)
@@ -67,30 +78,38 @@ def loop():
         board.digital_read(BUTTON1PIN)
         time.sleep(0.2)
 
-    while Timer != 0 and add_time != 0:
-        board.digital_write(pin_red, 1)
-        board.digital_write(pin_yellow, 0)
-        minutes = Timer // 60
-        seconds = Timer % 60
-        display_time = '{:02d}.{:02d}'.format(minutes, seconds)
-        Timer -= 1
-        
-        # Check if Timer is greater than 0, and change the order status to "In Preparation"
-        if Timer > 0:
-            update_status_on_kitchen_server('in_preparation')
-            
-        time.sleep(1)
-        board.displayShow(display_time)
+    orders = fetch_orders_from_server()
 
-    board.displayShow('0000')
-    board.digital_write(pin_green, 1)
-    board.digital_write(pin_red, 0)
-    board.digital_write(BUZZER, 1)
-    time.sleep(5)
-    board.digital_write(BUZZER, 0)
-    
-    # Change the order status to "Done" when Timer reaches 0
-    update_status_on_kitchen_server('done')
+    if not orders:
+        print("No orders available.")
+    else:
+        first_order = orders[0]  # Assuming orders is a list of dictionaries
+        order_index = first_order['order_index']
+
+        while Timer != 0 and add_time != 0:
+            board.digital_write(pin_red, 1)
+            board.digital_write(pin_yellow, 0)
+            minutes = Timer // 60
+            seconds = Timer % 60
+            display_time = '{:02d}.{:02d}'.format(minutes, seconds)
+            Timer -= 1
+
+            # Check if Timer is greater than 0, and change the order status to "In Preparation"
+            if Timer > 0:
+                update_status_on_kitchen_server(order_index, 'in_preparation')
+
+            time.sleep(1)
+            board.displayShow(display_time)
+
+        board.displayShow('0000')
+        board.digital_write(pin_green, 1)
+        board.digital_write(pin_red, 0)
+        board.digital_write(BUZZER, 1)
+        time.sleep(5)
+        board.digital_write(BUZZER, 0)
+
+        # Change the order status to "Done" when Timer reaches 0
+        update_status_on_kitchen_server(order_index, 'done')
 
 if __name__ == '__main__':
     setup()
@@ -98,6 +117,6 @@ if __name__ == '__main__':
         try:
             loop()
         except KeyboardInterrupt:
-            print ('Bye Bye')
+            print('Bye Bye')
             board.shutdown()
             sys.exit(0)
